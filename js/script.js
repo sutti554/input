35c87e77ec64ef980de8cf16edcfee763c585236
Isabelle <sutti554@newschool.edu>
2019-11-12 09:36:07 -0500

    adding output message

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
-const userInput = window.prompt('What is your favorite color?');
+const userInput = window.prompt('What is your favorite animal?');
+const userInputWhy = window.prompt('Why?');
 const userOutputEl = document.getElementById('output');
 
-// console.log(userInput);
-// console.log(userOutputEl)
+console.log(userInput);
+console.log(userOutputEl)
+
+userOutputEl.innerText = `Your favorite animal is the ${userInput} because ${userInputWhy}`;
 
