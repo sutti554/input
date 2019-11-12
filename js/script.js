da2c26dfba6800716e5813933f3d930617ccf1c1
Isabelle <sutti554@newschool.edu>
2019-11-12 09:18:05 -0500

    initial commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const userInput = window.prompt('What is your favorite color?');
+const userOutputEl = document.getElementById('output');
+
+// console.log(userInput);
+// console.log(userOutputEl)
+
